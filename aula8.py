c0d8857c48dad8e9be2a2f622196947baee76275
TheLuke <ricardofernandesdearaujo3@hotmail.com>
2025-08-16 02:39:07 -0300

    Aula 8 - Primeiro Exercício

---
+++ aula8.py
@@ -0,0 +1,16 @@
+# Declarando Variáveis
+nome = "Ricardo"
+sobrenome = "Fernandes de Araujo"
+idade = 18
+ano_nascimento = 2007
+maior_de_idade = idade >= 18
+altura_metros = 1.68
+
+# Resultados
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("Ano de nascimento:", ano_nascimento)
+print("É maior de idade?", maior_de_idade)
+print('Altura em metros:', altura_metros)
+
