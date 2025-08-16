97e21fef947196ea77586c1cd7d04280c2d62b90
TheLuke <ricardofernandesdearaujo3@hotmail.com>
2025-08-16 03:10:05 -0300

    Aula 15 - Aprendendo a Função Input

---
+++ aula15.py
@@ -0,0 +1,9 @@
+# Aprendendo a função input para receber dados do usuário
+
+nome = input('Qual é o seu nome? ')
+numero_1 = int(input('Digite um número: '))
+numero_2 = int(input('Digite outro número: '))
+
+print(f"O seu nome é {nome}")
+print(f"O seu nome é {nome=}")
+print(f"A soma dos numeros é: {numero_1 + numero_2}")
