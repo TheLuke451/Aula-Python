5386e8419bbfcb15347dec26343dd17fcd6285d3
TheLuke <ricardofernandesdearaujo3@hotmail.com>
2025-08-16 02:33:42 -0300

    Aula 1 - Introdução ao Python

---
+++ aula1.py
@@ -0,0 +1,15 @@
+
+""" 
+Aula 1 - Python Básico
+Conhecendo comentários novamente e Docstrings
+Comentários são usados para explicar o código e não são executados pelo Python.
+Docstrings são strings de documentação que descrevem o propósito de uma função, classe ou módulo.
+Comentários são iniciados com o símbolo # e vão até o final da linha.
+Docstrings são iniciadas e terminadas com três aspas (simples ou duplas)
+
+"""
+
+print(1+1)
+print("Olá, Mundo!")
+
+
