5b94855291996e8faf22e8f062d931b19301d3da
TheLuke <ricardofernandesdearaujo3@hotmail.com>
2025-08-16 02:35:20 -0300

    Aula 3 - Aprendendo mais sobre o Python e sobre Strings

---
+++ aula3.py
@@ -0,0 +1,28 @@
+
+"""
+Aula 3 - Python Básico
+
+Python = Linguagem de Programação
+Tipo de tipagem = Dinâmica/Forte
+str = String (Texto)
+Strings são sequências de caracteres usadas para representar texto.
+exemplo de string: "Olá, Mundo!"
+Tipagem Dinâmica significa que a linguagem não exige que você declare o tipo de uma variável antes de usá-la.
+
+"""
+
+print(1234)
+
+
+# Aspas simples
+print('Olá, Mundo!')
+print(1, 'Olá, "Mundo"!')
+
+# Aspas duplas
+print("Olá, Mundo!")
+
+# Escape
+print("Olá, \"Mundo\"!")
+
+# R
+print(r"Olá, \"Mundo\"!") # Raw string, não processa escape
