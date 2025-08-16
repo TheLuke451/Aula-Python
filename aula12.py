674c341357c0ad210b1b6dc01a00999d4c6547fa
TheLuke <ricardofernandesdearaujo3@hotmail.com>
2025-08-16 02:42:06 -0300

    Aula 12 - Exercício de Cálculo do IMC

---
+++ aula12.py
@@ -0,0 +1,10 @@
+#Exercício de cálculo do IMC
+# Cálculo do Índice de Massa Corporal (IMC)
+nome = "Ricardo Fernandes de Araujo"
+peso = 63.10
+altura = 1.68
+imc = peso / (altura ** 2)
+
+print(nome,"tem", peso, "kg e", altura, "m de altura")
+print("pesa",peso, "quilos e seu imc é:")
+print(imc)
