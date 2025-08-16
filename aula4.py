62826296cdb6873a49b9c6e4e76625f872d98702
TheLuke <ricardofernandesdearaujo3@hotmail.com>
2025-08-16 02:36:00 -0300

    Aula 4 - Tipos Int e Float

---
+++ aula4.py
@@ -0,0 +1,18 @@
+# Tipos int e float
+
+# int -> Números inteiros
+# O tipo int é usado para representar números inteiros, ou seja, números sem parte decimal.
+print(11) # int
+print(-11) #int
+print(0) # int
+
+# float -> Números de ponto flutuante
+# O tipo float é usado para representar números com parte decimal, ou seja, números reais.
+print(1.1, 10.11) # float
+print(0.0, -1.5) # float
+
+# A função type é usada para verificar o tipo de uma variável.
+print(type('Otávio'))
+print(type(0))
+print(type(1.1), type(-1.1), type(0.0))
+
