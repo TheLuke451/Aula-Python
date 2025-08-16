984b8d51c045133ad379e99970cf65663e234715
TheLuke <ricardofernandesdearaujo3@hotmail.com>
2025-08-16 02:39:43 -0300

    Aula 9 - Operadores Aritméticos

---
+++ aula9.py
@@ -0,0 +1,23 @@
+
+# Operadores Aritméticos
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 30 - 5
+print("Subtração", subtracao)
+
+multiplicacao = 10 * 10
+print("Multiplicação", multiplicacao)
+
+divisao = 10 / 3
+print("Divisão", divisao)
+
+divisao_inteira = 10 // 3
+print("Divisão inteira", divisao_inteira)
+
+exponenciacao = 2 ** 3
+print("Exponenciação", exponenciacao)
+
+modulo = 10 % 3
+print("Módulo", modulo)
+
