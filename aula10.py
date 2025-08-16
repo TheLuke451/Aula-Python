0bd1470a2abdedaa86989e118bc14844a9f63d41
TheLuke <ricardofernandesdearaujo3@hotmail.com>
2025-08-16 02:40:33 -0300

    Aula 10 - Concatenação de Strings

---
+++ aula10.py
@@ -0,0 +1,12 @@
+
+# Concatenacao de strings
+# Concatenar significa juntar, unir, ligar.
+# No Python, podemos concatenar strings usando o operador +.
+
+concatenacao = 'A' + 'B' + 'C'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_ricardo = 'Ricardo' * 3
+print(a_dez_vezes)
+print(tres_vezes_ricardo)
