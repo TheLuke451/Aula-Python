b86cf2e8d0f15838cb0fff84ddcd5a2da6c198c6
TheLuke <ricardofernandesdearaujo3@hotmail.com>
2025-08-16 02:34:35 -0300

    Aula 2 - Aprendendo sobre a Função Print

---
+++ aula2.py
@@ -0,0 +1,19 @@
+
+"""
+Aula 2 - Python Básico
+Conhecendo a função print e como imprimir múltiplos valores
+A função print é usada para exibir informações na tela.
+A função print pode receber múltiplos valores separados por vírgula, que serão exibidos na mesma linha, separados por espaço.
+A função print também permite personalizar o separador entre os valores usando o parâmetro 'sep'.
+
+"""
+
+# \r\n -> CRLF (Quebra de linha no Windows)
+# \n -> LF (Quebra de linha no Linux e Mac)
+
+print(12, 34, sep="-", end='\n')
+print(56, 78, sep='-',end='\n')
+print(9, 10, sep='-',end='\n')
+
+
+
