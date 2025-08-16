ed27f06bb61c95036e18e41a48affb442b846f20
TheLuke <ricardofernandesdearaujo3@hotmail.com>
2025-08-16 02:36:31 -0300

    Aula 5 - Tipo Booleano

---
+++ aula5.py
@@ -0,0 +1,15 @@
+
+# Tipo de dado Boolean
+# Boolean -> Verdadeiro ou Falso
+# O tipo boolean é usado para representar valores lógicos, ou seja, verdadeiro (True) ou falso (False).
+# Existem vários operadores para "questionar"
+# Dentre eles o == (igualdade) e o != (diferente)
+
+# Com simbolo de igualdade
+print( 10 == 10 ) # Sim => True (Verdadeiro)
+print( 10 == 11 ) # Não => False (Falso)
+
+# Com símbolo de diferente
+print( 10 != 10 ) # Não => False (Falso)
+print( 10 != 11 ) # Sim => True (Verdadeiro)
+
