08e2b5693b82ba8607f0b14fa5da46f7f3d9cf23
TheLuke <ricardofernandesdearaujo3@hotmail.com>
2025-08-16 02:37:30 -0300

    Aula 6 - Conversão de tipos de dados e Concatenação

---
+++ aula6.py
@@ -0,0 +1,16 @@
+
+# Conversão de tipos, coerção
+# Type Convertion, typecasting, coercion
+# É o ato de converter um tipo de dado em outro tipo de dado.
+# Tipos imutáveis e primitivos:
+# str, int, float, bool
+
+print(1 + 1)
+print( "a" + "b" )
+print('1' + '1')
+
+print(int('1'), type(int('1')))
+print(int('1')+ 1)
+print(float('1') + 1)
+print(bool(' '))
+print(str(11) + 'b')
