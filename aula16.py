6363e6bfe7f714630482ecffe24385dc825f49b4
TheLuke <ricardofernandesdearaujo3@hotmail.com>
2025-08-17 08:37:12 -0300

    Aula 16 - Aprendendo sobre Operadores Condicionais

---
+++ aula16.py
@@ -0,0 +1,10 @@
+# Aprendendo sobre operações condicionais IF, ELIF e ELSE (Se, Senão, Senão se)
+entrada = input("Você quer 'entrar' ou 'sair'? ")
+
+if entrada == 'entrar':
+    print("Você entrou no sistema.")
+elif entrada == 'sair':
+    print("Você saiu do sistema.")
+else:
+    print("Opção inválida. Por favor, digite 'entrar' ou 'sair'.")
+    
