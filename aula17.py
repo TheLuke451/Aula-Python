e000025d2b58525aef33a40f6966b906f4aaea5e
TheLuke <ricardofernandesdearaujo3@hotmail.com>
2025-08-17 08:48:26 -0300

    Aula 17 - PARTE 2 Sobre Operadores Condicionais

---
+++ aula17.py
@@ -0,0 +1,26 @@
+# PARTE 2 Sobre Operadores Condicionais
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = False
+
+
+if condicao1:
+    print("Código para condição 1")
+elif condicao2:
+    print("Código para condição 2")
+elif condicao3:
+    print("Código para condição 3")
+elif condicao4:
+    print("Código para condição 4")
+else: 
+    print("Nenhuma condição foi satisfeita")
+
+# ==============================================================
+
+if 10 == 10:
+    print("IF")
+else:
+    print("Else")
+
+print("Fora do IF")
