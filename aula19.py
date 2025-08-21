dcf14e625b37b45488c1bd6c70ac636a83f08eaa
TheLuke <ricardofernandesdearaujo3@hotmail.com>
2025-08-21 13:43:49 -0300

    Aula 19 - Operadores Relacionais

---
+++ aula19.py
@@ -0,0 +1,19 @@
+"""
+Operadores Relacionais (Comparação)
+Operadores relacionais são usados para comparar valores. Eles retornam um valor booleano (True ou False).
+Operadores:
+- Igual: `==`
+- Diferente: `!=`
+- Maior que: `>`
+- Menor que: `<`
+- Maior ou igual a: `>=`
+- Menor ou igual a: `<=`
+"""
+
+maior = 10 > 5
+maior_igual = 10 >= 10
+menor = 10 < 5
+menor_igual = 10 <= 5
+igual = 10 == 10
+diferente = 10 != 5
+print(diferente)
