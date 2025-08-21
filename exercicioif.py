d5a54e3d7871b5c0f83a0e9d497ec85661ae6578
TheLuke <ricardofernandesdearaujo3@hotmail.com>
2025-08-21 13:49:13 -0300

    Exercício com Operador Condicional e Relacional

---
+++ exercicioif.py
@@ -0,0 +1,7 @@
+primeirovalor = int(input("Digite o primeiro valor: "))
+segundovalor = int(input("Digite o segundo valor: "))
+
+if primeirovalor > segundovalor:
+    print(f"{primeirovalor} é maior que {segundovalor}")
+else:
+    print(f"{segundovalor} é maior que {primeirovalor}")
