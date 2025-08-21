d1798bad57e5237a90b811a462f48196d092e8c1
TheLuke <ricardofernandesdearaujo3@hotmail.com>
2025-08-21 14:31:16 -0300

    Aula 20 - Operadores Lógicos com 'and'.

---
+++ aula20.py
@@ -0,0 +1,22 @@
+"""
+Operadores Lógicos
+Operadores lógicos são usados para combinar expressões booleanas. Os principais operadores lógicos em Python são:
+- `and`: Retorna True se ambas as expressões forem verdadeiras.
+- `or`: Retorna True se pelo menos uma das expressões for verdadeira.
+- `not`: Inverte o valor lógico da expressão.
+- `None`: Representa a ausência de valor ou um valor nulo. É um tipo especial em Python.
+"""
+
+entrada = input("[E]ntrar [S]air: ")
+senha_digitada = input("Senha: ")
+senha_permitida = "1234"
+
+if entrada == "E" and senha_digitada == senha_permitida:
+    print("Você escolheu entrar.")
+elif entrada == "E" and senha_digitada != senha_permitida:
+    print("Senha incorreta. Acesso negado.")
+elif entrada == "S":
+    print("Você escolheu sair.")
+else:
+    print("Opção Inválida. Por favor, escolha 'E' para entrar ou 'S' para sair.")
+
