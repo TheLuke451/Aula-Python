536df0a8cdd5a6a6c77e24758f82ee786eed6934
TheLuke <ricardofernandesdearaujo3@hotmail.com>
2025-08-29 17:59:24 -0300

    Aula 22 - Operadores in e Not in

---
+++ aula22.py
@@ -0,0 +1,14 @@
+"""
+Operadores in e not in
+Strings são iteráveis, ou seja, podemos acessar seus elementos através do índice.
+"""
+
+nome = "Ricardo"
+print(nome[2]) # Output: c
+print(nome[-1]) # Output: o
+
+print('a' in nome) # Output: True
+print('z' in nome) # Output: False
+
+
+print("cardo" not in nome) # Output: False
